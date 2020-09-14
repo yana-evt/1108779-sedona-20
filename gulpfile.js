67de761d23d2dfd65d1a162bcb2629e4950b867a
yana-evt <iana.evtukhova@gmail.com>
2020-09-14 22:22:49 +0200

    Начинаем подготовку адаптивной графики в личном проекте

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,11 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const webpicture = require("gulp-webp");
+const del = require("del");
 
 // Styles
 
@@ -16,12 +21,61 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.svgo()
+    ]))
+}
+
+exports.images = images;
+
+// WebP
+
+const webp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webpicture({quality: 90}))
+    .pipe(gulp.dest("source/img"))
+}
+
+exports.webp = webp;
+
+// Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff, woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+    ], {
+    base: "source"
+    })
+    .pipe(gulp.dest("build"));
+}
+
+exports.copy = copy;
+
+// Del
+
+const clean = () => {
+  return del("build");
+}
+
+exports.clean = clean;
 
 // Server
 
